a0a4b50297574892d18dd1827a06a38c71244fd0
洪嘉勇 <tjhjyisland@gmail.com>
2016-10-25 10:31:51 +0800

    同心云轻应用部署于Stanforxc的服务器上 远端数据库部署于Charon的服务器上

---
+++ xianhuo_tongxinyun/src/main/java/com/example/entity/File.java
@@ -0,0 +1,62 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by deado on 2016/10/22.
+ */
+@Entity @Table(name = "FILE", schema = "test")
+public class File {
+    @Id @Column(nullable = false, name = "ID")
+    private String ID;//??
+    @Column(nullable = false, name = "NAME")
+    private String NAME;
+    @Column(nullable = false, name = "URL")
+    private String URL;
+    @Column(nullable = false, name = "CLUB")
+    private String CLUB;
+
+    public File(String ID, String NAME, String URL, String CLUB) {
+        this.ID = ID;
+        this.NAME = NAME;
+        this.URL = URL;
+        this.CLUB = CLUB;
+    }
+
+    // data get & set way
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public String getCLUB() {
+        return CLUB;
+    }
+
+    public void setCLUB(String CLUB) {
+        this.CLUB = CLUB;
+    }
+}
---
+++ xianhuo_tongxinyun/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,72 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by hongjiayong on 2016/10/17.
+ */
+@Entity @Table(name = "STUDENT", schema = "test")
+public class Student{
+
+    @Id @Column(nullable = false, name = "ID")
+    private String ID;
+    @Column(nullable = false, name = "NAME")
+    private String NAME;
+    @Column(nullable = true, name = "GRADE")
+    private String GRADE;
+    @Column(nullable = true, name = "MAJOR")
+    private String MAJOR;
+    @Column(nullable = true, name = "CONTACT")
+    private String CONTACT;
+
+    public Student(String ID, String NAME, String GRADE, String MAJOR, String CONTACT) {
+        this.ID = ID;
+        this.NAME = NAME;
+        this.GRADE = GRADE;
+        this.MAJOR = MAJOR;
+        this.CONTACT = CONTACT;
+    }
+
+    public String getGRADE() {
+        return GRADE;
+    }
+
+    public void setGRADE(String GRADE) {
+        this.GRADE = GRADE;
+    }
+
+    public String getMAJOR() {
+        return MAJOR;
+    }
+
+    public void setMAJOR(String MAJOR) {
+        this.MAJOR = MAJOR;
+    }
+
+    public String getCONTACT() {
+        return CONTACT;
+    }
+
+    public void setCONTACT(String CONTACT) {
+        this.CONTACT = CONTACT;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
