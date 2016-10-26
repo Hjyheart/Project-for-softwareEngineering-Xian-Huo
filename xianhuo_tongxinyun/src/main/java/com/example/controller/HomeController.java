811fa2c6c74f81145a69c3d7d702fd988d4fc7cb
洪嘉勇 <tjhjyisland@gmail.com>
2016-10-27 00:30:27 +0800

    完成activity页的列表 使用ajax解决局部刷新 刷新方式采用下拉刷新！

--- xianhuo_tongxinyun/src/main/java/com/example/controller/HomeController.java
+++ xianhuo_tongxinyun/src/main/java/com/example/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 /**
  * Created by hongjiayong on 2016/10/25.
  */
@@ -18,7 +20,39 @@ public class HomeController {
         map.addAttribute("sex", "男");
         map.addAttribute("studentId", "1452822");
 
+        ArrayList<testUser> userList = new ArrayList<>();
+
+        userList.add(new testUser("洪嘉勇", 21, "无敌"));
+        userList.add(new testUser("李阳", 20, "全能王"));
+
+        map.addAttribute("users", userList);
+
         return "home";
+    }
+
+
+    public class testUser{
+        private String name;
+        private int age;
+        private String description;
+
+        public testUser(String name, int age, String description){
+            this.name = name;
+            this.age = age;
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
     }
 
 
