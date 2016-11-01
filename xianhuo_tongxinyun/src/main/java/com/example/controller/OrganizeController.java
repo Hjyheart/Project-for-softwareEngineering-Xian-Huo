0984dfd1f1a94f02a07bd7376da511eda1a6afde
洪嘉勇 <tjhjyisland@gmail.com>
2016-11-01 16:41:48 +0800

    添加搜索栏 完成我的社团和我的活动和具体社团和活动的连接

--- xianhuo_tongxinyun/src/main/java/com/example/controller/OrganizeController.java
+++ xianhuo_tongxinyun/src/main/java/com/example/controller/OrganizeController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 
@@ -46,6 +48,52 @@ public class OrganizeController {
         map.addAttribute("name", name);
 
         return "organize";
+    }
+
+    @RequestMapping(value = "/{name}/apply")
+    // 申请参加社团
+    public @ResponseBody String applyOrganize(@PathVariable String name, ModelMap map, HttpServletRequest request){
+        System.out.println(name);
+
+        return "true";
+    }
+
+    @RequestMapping(value = "/{name}/unapply")
+    // 取消申请社团
+    public @ResponseBody String unApplyActivity(@PathVariable String name, ModelMap map, HttpServletRequest request){
+        System.out.println(name);
+
+        return "true";
+    }
+
+    @RequestMapping(value = "/{name}/comment", method = RequestMethod.GET)
+    // 进行评论
+    public @ResponseBody String commentOrganize(@PathVariable String name, String comment, HttpServletRequest servletRequest){
+        System.out.println(comment);
+
+        return "true";
+    }
+
+    @RequestMapping(value = "/{name}/comment/refresh", method = RequestMethod.GET)
+    // 异步加载评论
+    public @ResponseBody ArrayList<ActivityController.activity> commentRefresh(@PathVariable String name, int start, int number, HttpServletRequest request){
+        ArrayList<ActivityController.activity> activityList = new ArrayList<>();
+        activityList.add(new ActivityController.activity("liyang", "wudi", "lala"));
+        activityList.add(new ActivityController.activity("liyang", "lala", "lala"));
+
+        return activityList;
+    }
+
+    @RequestMapping(value = "/{name}/good")
+    // 点赞
+    public @ResponseBody String goodForOrganize(@PathVariable String name, ModelMap map, HttpServletRequest  request){
+        return "false";
+    }
+
+    @RequestMapping(value = "/{name}/ungood")
+    // 取消点赞
+    public @ResponseBody String unGoodForOrganize(@PathVariable String name, ModelMap map, HttpServletRequest request){
+        return "true";
     }
 }
 
