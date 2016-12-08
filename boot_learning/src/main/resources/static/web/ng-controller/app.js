cea9da4d39a27fa9b9e6807b8c69ec9743994857
洪嘉勇 <tjhjyisland@gmail.com>
2016-12-09 00:42:02 +0800

    j2ee前端移植

---
+++ boot_learning/src/main/resources/static/web/ng-controller/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by hongjiayong on 2016/12/8.
+ */
+var app = angular.module('myApp', []);
---
+++ boot_learning/src/main/resources/static/web/ng-controller/homeCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hongjiayong on 2016/12/8.
+ */
+app.controller('homeCtrl', ['$scope', '$http', function($scope, $http){
+    this.$onInit = function(){
+        console.log('success');
+    };
+}]);
